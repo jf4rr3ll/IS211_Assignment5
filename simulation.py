69c4dd4097144cbfb20b23df3e208c6edf9c87eb
jf4rr3ll <jf4rr3ll@gmail.com>
2016-10-03 20:21:59 -0400

    Completed assignment

--- simulation.py
+++ simulation.py
@@ -8,18 +8,23 @@ import csv
 import argparse
 
 class Request:
-    def __init__(self, request):
-        self.timestamp = request[0]
-        self.time = request[2]
+    def __init__(self, req_time, req_url, req_process):
+        self.req_time = int(req_time)
+        self.req_url = req_url
+        self.req_process = int(req_process)
+        self.waitTime = 0
 
-    def get_stamp(self):
-        return self.timestamp
+    def getTime(self):
+        return self.req_time
 
-    def get_time(self):
-        return self.time
+    def getRequestURL(self):
+        return self.req_url
 
-    def wait_time(self, current_time):
-        return current_time - self.timestamp
+    def getProcessTime(self):
+        return self.req_process
+
+    def getWaitTime(self, req_time):
+        return self.req_time - self.req_process
 
 
 class Server:
@@ -40,7 +45,7 @@ class Server:
 
     def start_next(self,new_task):
         self.current_task = new_task
-        self.time_remaining = new_task.get_time()
+        self.time_remaining = new_task.getTime()
 
 
 class Queue:
@@ -62,34 +67,65 @@ class Queue:
 
 def simulateOneServer(filename):
     """Defines a function to print the average wait time for the request"""
-    return avg
+    server = Server()
+    server_queue = Queue()
+    waiting_times = []
+
+    for i in filename:
+        req_sec = i[0]
+        req_url = i[1]
+        req_process = i[2]
+        request = Request(req_sec, req_url, req_process)
+        server_queue.enqueue(request)
+
+        if (not server.busy()) and (not server_queue.is_empty()):
+            next_request = server_queue.dequeue()
+            waiting_times.append(next_request.getWaitTime(req_sec))
+            server.start_next(next_request)
+
+        server.tick()
+
+    average_wait = sum(waiting_times) / len(waiting_times)
+
+    print("Average Wait %6.2f secs for %3d tasks remaining." % (average_wait, server_queue.size()))
 
 
-def simulateManyServers(filename):
+def simulateManyServers(filename, server_num):
     """Defines a function to print the average wait time for multiple servers with load balancing"""
-    return avg
+    server = [Server() * server_num]
+    server_queue = Queue()
+    waiting_times = []
 
+    for i in filename:
+        req_sec = i[0]
+        req_url = i[1]
+        req_process = i[2]
+        request = Request(req_sec, req_url, req_process)
+        server_queue.enqueue(request)
+
+    for i in server:
+        if (not server.busy()) and (not server_queue.is_empty()):
+            next_request = server_queue.dequeue()
+            waiting_times.append(next_request.getWaitTime(req_sec))
+            server.start_next(next_request)
+
+        server.tick()
+
+    average_wait = sum(waiting_times) / len(waiting_times)
+
+    print("Average Wait %6.2f secs for %3d tasks remaining." % (average_wait, server_queue.size()))
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", help="Enter the filename to get the CSV file.")
     parser.add_argument("--server", help="Enter the number of servers for load balancing.")
     args = parser.parse_args()
+    csvData = csv.reader(urllib2.urlopen(args.file))
 
-    if args.file:
-        try:
-            csvData = csv.reader(urllib2.urlopen(args.file))
-            simulateOneServer(csvData)
-
-        except urllib2.URLError as URLError:
-            print "This URL entered is invalid."
-            raise URLError
-    elif args.servers:
-        try:
-            csvData = csv.reader(urllib2.urlopen(args.server))
-            simulateManyServers(csvData)
-    else:
-        print "Please enter a valid URL."
+    if not args.server:
+        simulateOneServer(csvData)
+    else :
+        simulateManyServers(csvData)
 
 if __name__ == "__main__":
     main()
